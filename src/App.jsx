632f5ef130671aa49d5735db8356c1515e32e9eb
jas04-11 <140260520+jas04-11@users.noreply.github.com>
2024-11-10 13:00:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+import Mainpage from './Component/Mainpage'
+import {Route, Routes} from "react-router-dom";
+import Mealinfo from './Component/Mealinfo';
+
+function App() {
+  return (
+      //<Mainpage/>
+      <Routes>
+        <Route path="/" element={<Mainpage/>}/>
+        <Route path="/:mealid" element={<Mealinfo/>}/>
+      </Routes>
+  )
+}
+
+export default App
